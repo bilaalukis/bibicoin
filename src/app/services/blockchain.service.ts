fa651a3004ba1dfadfe88d4c001de3849bb4bae0
bilaalukis <bilaalukis123@gmail.com>
2021-04-04 23:17:02 +0900

    initial commit

---
+++ src/app/services/blockchain.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+import { Blockchain } from "blockchain_project/src/blockchain"
+@Injectable({
+  providedIn: 'root',
+})
+export class BlockchainService {
+  constructor() {}
+}
