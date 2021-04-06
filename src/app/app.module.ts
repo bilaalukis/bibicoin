d6fd0c79dcf28737ff0792652b396ba95b4166d5
bilaalukis <bilaalukis123@gmail.com>
2021-04-06 10:18:24 +0900

    Created frontend to display each block and its transaction on the blockchain created from project-blockchain

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlockchainViewerComponent } from './pages/blockchain-viewer/blockchain-viewer.component';
+import { BlockViewComponent } from './components/block-view/block-view.component';
+import { TransactionsTableComponent } from './components/transactions-table/transactions-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlockchainViewerComponent,
+    BlockViewComponent,
+    TransactionsTableComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
